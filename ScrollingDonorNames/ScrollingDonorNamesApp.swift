163ffd190fdee42bcd85872a8815dae6e255aef9
Rafael Schmitt <rafael.claycon@gmail.com>
2023-04-14 13:50:41 -0300

    Initial Commit

---
+++ ScrollingDonorNames/ScrollingDonorNamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingDonorNamesApp.swift
+//  ScrollingDonorNames
+//
+//  Created by Rafael Schmitt on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollingDonorNamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
